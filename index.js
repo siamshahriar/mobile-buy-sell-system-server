deefe68b0194b2e3bdd377c74120e6b7ea7194cb
siamshahriar <siamshahriar99@gmail.com>
2022-11-28 21:08:03 +0600

    to get the category names

--- index.js
+++ index.js
@@ -13,6 +13,42 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+/* --- MongoDB ---- */
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dhw1j4v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const categoryCollection = client
+      .db("mobileBuySell")
+      .collection("category");
+    const productsCollection = client
+      .db("mobileBuySell")
+      .collection("products");
+    const usersCollection = client.db("mobileBuySell").collection("users");
+
+    //to get the category names
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const result = await categoryCollection.find(query).toArray();
+      //   console.log(result);
+      res.send(result);
+    });
+
+ 
+
+   
+  } finally {
+  }
+}
+
+run().catch(console.log);
+
 //Basic server start
 app.get("/", async (req, res) => {
   res.send("mobile buy sell server is running");
