8ecc7a969b83067e802217073eeae523c24e592f
siamshahriar <siamshahriar99@gmail.com>
2022-11-29 15:46:31 +0600

    product delete works now

--- index.js
+++ index.js
@@ -127,6 +127,14 @@ async function run() {
       const result = await productsCollection.find(query).toArray();
       res.send(result);
     });
+
+    //it will delete the product based on id
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
